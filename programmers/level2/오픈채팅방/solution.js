86a71bbf3fbc52f450ad59f460dba5b829a9d17d
한광재 <hkj4900@pusan.ac.kr>
2022-01-07 16:13:16 +0900

    Fix: 코드 수정

--- programmers/level2/오픈채팅방/solution.js
+++ programmers/level2/오픈채팅방/solution.js
@@ -1,49 +1,16 @@
 function solution(record) {
-  var answer = [];
-  
-  let users = {};
-  
-  let history = [];
-  
-  for (let r of record) {
-      const info = r.split(" ");
-      const act = info[0];
-      const uid = info[1];
-      
-      if (act === "Enter") {
-          const nickName = info[2]
-          if (!users[uid]) {
-              users[uid] = new User(nickName, uid);
-          } else {
-              users[uid].changeNickName(nickName);
-          }
-          history.push([act, users[uid]]);
-      } else if (act === "Change") {
-          const nickName = info[2]
-          users[uid].changeNickName(nickName);
-      } else {
-          history.push([act, users[uid]]);
-      }    
-  }
-  
-  for (let h of history) {
-      if (h[0] === "Enter") {
-          answer.push(`${h[1].nickName}님이 들어왔습니다.`);
-      } else {
-          answer.push(`${h[1].nickName}님이 나갔습니다.`);
-      }
-  }
-  
-  return answer;
-}
+    const users = {}
+    const history = []
+    const message = ([command, uid]) => {
+        return `${users[uid]}님이 ${command === 'Enter' ? '들어왔습니다.' : '나갔습니다.'}`
+    }
+    record.forEach((v) => {
+        const [command, uid, nickname] = v.split(' ')
 
-class User {
-  constructor(nickName, uid) {
-      this.nickName = nickName;
-      this.uid = uid
-  }
-  
-  changeNickName(_nickName) {
-      this.nickName = _nickName;
-  }
+        if (command !== 'Change') {
+            history.push([command, uid])
+        }
+        if (nickname) users[uid] = nickname
+    })
+    return history.map(v => message(v))
 }
