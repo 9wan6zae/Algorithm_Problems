2463f6da82cec2611acf82934fbd098f56af98b5
한광재 <hkj4900@pusan.ac.kr>
2022-05-03 10:42:04 +0900

    Solve: 2 x n 타일링

---
+++ programmers/level3/2 x n 타일링/solution.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  const dp = []
+  
+  dp[0] = 1
+  dp[1] = 1
+  
+  for (let i = 2; i <= n; i++) {
+      dp[i] = (dp[i-1] + dp[i-2])%1000000007
+  }
+  
+  return dp[n];
+}
