fddcef9e8cdff5f4b0273481fc53e15206e6f01d
한광재 <hkj4900@pusan.ac.kr>
2022-05-13 18:57:51 +0900

    Solve: 최고의 집합

--- programmers/level3/최고의 집합/solution.js
+++ programmers/level3/최고의 집합/solution.js
@@ -1,7 +1,7 @@
 function solution(n, s) {
-  if (s === 1 || s < n) return [-1]
+  if (s < n) return [-1]
   
-  let array = Array(n).fill(0).map(el => el = Math.floor(s / n));
+  const array = Array(n).fill(0).map(el => el = Math.floor(s / n));
   for (let i = n - 1; i >= n - (s % n); i--) {
       array[i]++;
   }
